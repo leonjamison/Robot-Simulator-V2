8568ae8157c8b2323c213720d371cceb3373910d
Leon Jamison <leonjamison@icloud.com>
2020-03-12 11:46:40 -0400

    Working progress

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(){
+  
+  const moves = []
+
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+
+  const moveButton = document.querySelector("#move-button")
+  moveButton.addEventListener("click", function(e){
+    const direction = moves.shift()
+    if (direction){
+      move(direction)
+      const moveList = document.querySelector("#moves-container")
+      moveList.firstElementChild.remove()
+    } else {
+      console.log("Done!")
+    }
+  })
 })
+
+  document.addEventListener("keydown", function(e){
+    e.preventDefault()
+
+  }
+
+
+
+
+
+
+
+  
+}
